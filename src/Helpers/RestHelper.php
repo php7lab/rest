0dba6dd01f03c03a8b2862345fbbe52df5f6ea82
Vitaliy <vitaliy_krg@rocketfirm.com>
2020-02-11 11:52:56 +0600

    fix: return method

--- src/Helpers/RestHelper.php
+++ src/Helpers/RestHelper.php
@@ -16,9 +16,21 @@ use Symfony\Component\Routing\Route;
 use Symfony\Component\Routing\RouteCollection;
 use Symfony\Component\Routing\Matcher\UrlMatcher;
 use Symfony\Component\Routing\RequestContext;
+use PhpLab\Core\Enums\Http\HttpHeaderEnum;
+use Psr\Http\Message\ResponseInterface;
 
 class RestHelper
 {
+
+    public static function getDataFromResponse(ResponseInterface $response)
+    {
+        $contentType = $response->getHeaderLine(HttpHeaderEnum::CONTENT_TYPE);
+        $data = $response->getBody()->getContents();
+        if($contentType == 'application/json') {
+            $data = json_decode($data, true);
+        }
+        return $data;
+    }
 
     public static function defineCrudRoutes(string $endpoint, string $controllerClassName): RouteCollection
     {
