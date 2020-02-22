155cd75acdae277b7d59c593859ca7cb183b2bec
Vitaliy <theyamshikov@yandex.ru>
2020-02-23 02:46:12 +0600

    feat: make test

---
+++ tests/ExampleTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace PhpLab\Rest\Tests;
+
+use PHPUnit\Framework\TestCase;
+
+final class ExampleTest extends TestCase {
+
+    public function testExample() {
+        $this->assertSame(1, 1);
+    }
+
+}
